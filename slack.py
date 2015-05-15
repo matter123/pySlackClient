0ba1aea508e51bd32c2360f6655cbd0e1e5f7491
Matthew Fosdick <matthew.fosdick@gmail.com>
2015-05-15 05:52:58 -0700

    more ap around ed, ping pong

--- slack.py
+++ slack.py
@@ -2,15 +2,46 @@ import websocket
 import requests
 
 import json
-import threading.Timer as Timer
+from threading import Timer
+
+
+class EventFilter(object):
+    def _inv_match(self, data):
+        return not self._inv_match_func(data)
+
+    def __init__(self, match_func=lambda x: True, invert=False):
+        super(EventFilter, self).__init__()
+        if invert:
+            self.match_func = self._inv_match
+            self._inv_match = match_func
+        else:
+            self.match_func = match_func
+        self._attach = []
+        self._attachprime = []
+
+    def attach(self, callback, prime=False):
+        if prime:
+            self._attachprime.append(callback)
+            return
+        self._attach.append(callback)
+
+    def dispatch(self, data):
+        if self.match_func(data):
+            for atch in self._attach:
+                atch(data)
+        else:
+            for atch in self._attachprime:
+                atch(data)
 
 
 class Slack(object):
     def __init__(self, team, email=None, passw=None, token=None,
-    	         userAgent="Mozilla/5.0 PySlack Client"):
+                 useragent="Mozilla/5.0 PySlack Client"):
+        self.connected = False
         self.team_url = 'https://' + team + '.slack.com'
         self.ok = True
-        self.userAgent = userAgent
+        self.userAgent = useragent
+        self.events = EventFilter()
         if token is not None:
             self.token = token
             print('connecting')
@@ -97,19 +128,47 @@ class Slack(object):
     def parse_boot_data(self):
         self._ws_url = self._boot_data['url']
 
+    def _message(self, message):
+        print(message)
+        if 'ok' in message and message['ok'] is not True:
+            print(message['error'])
+            return
+        if 'reply_to' in message:
+            if message['reply_to'] in self.track:
+                self.track.remove(message['reply_to'])
+            print(len(self.track))
+            return
+        self.events.dispatch(message)
+
     def setup_ws(self):
         self.ws = websocket.create_connection(self._ws_url)
+        self.track = []
+        self.nid = 1
 
-    def sendEvent(self, Type, data=None, track=False):
-    	eid = self.getNextId()
-    	self.ws.send(json.dumps({
-    			'type': Type,
-    			'id': eid
-    		}))
+    def _get_next_id(self):
+        temp = self.nid
+        self.nid += 1
+        return temp
+
+    def send_event(self, event_type, data=None, track=False):
+        eid = self._get_next_id()
+        if track:
+            self.track.append(eid)
+        args = {
+            'type': event_type,
+            'id': eid,
+        }
+        if data:
+            args.update(data)
+        self.ws.send(json.dumps(args))
 
     def _send_ping(self):
-    	Timer(2.00, send_ping).start()
-    	self.send_event("ping", track=True)
+        Timer(5.00, self._send_ping).start()
+        self.send_event("ping", track=True)
 
     def setup_ping(self):
-        Timer(2.00, self._send_ping).start()
+        Timer(5.00, self._send_ping).start()
+
+    def pass_control(self):
+        while True:
+            self._message(json.loads(self.ws.recv()))
